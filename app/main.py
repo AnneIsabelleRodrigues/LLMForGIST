108a1018eb6ccfa9a0647cca18ab9bdf42abf0b5
Anne Isabelle Rodrigues de Carvalho <anneisabelle.rodrigues@outlook.com>
2025-10-10 20:48:02 -0300

    Fine Tunning ajustado. Agora roda na minha máquina

--- app/main.py
+++ app/main.py
@@ -4,25 +4,26 @@ from config import QDRANT_CLUSTER_URL, QDRANT_API_KEY, HUGGINGFACE_TOKEN
 from models.dataset_loader import load_local_dataset
 from models.base_model import load_base_model
 from models.fine_tune import fine_tune
-from models.vector_db import init_qdrant, add_documents, load_texts_from_file
+from models.vector_db import init_qdrant, add_documents, load_documents_for_embedding, chunk_texts
 from models.rag_pipeline import retrieve_relevant_docs, generate_chat_answer
 
 if __name__ == '__main__':
 
     tokenizer, model = load_base_model()
 
-    # dataset = load_local_dataset("data/dataset")
-    #
-    # model = fine_tune(model, tokenizer, dataset)
+    dataset = load_local_dataset("data/dataset")
 
-    client = init_qdrant(url=QDRANT_CLUSTER_URL, api_key=QDRANT_API_KEY)
+    model_ft = fine_tune(model, tokenizer, dataset)
 
-    texts = load_texts_from_file("data/documents/summary.txt")
+    client = init_qdrant(url=QDRANT_CLUSTER_URL, api_key=QDRANT_API_KEY, recreate=True)
+
+    texts = load_documents_for_embedding("data/documents/")
+    chunks = chunk_texts(texts, chunk_size=1024, chunk_overlap=100)
     add_documents(client, texts)
 
     query = "Em poucas palavras, o que é GIST?"
     docs = retrieve_relevant_docs(client, query)
-    print(docs)
+
     answer = generate_chat_answer(model, tokenizer, query, docs)
 
     print("\nResposta:\n", answer)
--- app/models/fine_tune.py
+++ app/models/fine_tune.py
@@ -1,6 +1,6 @@
-# app/models/fine_tune.py
 from transformers import Trainer, TrainingArguments
 from peft import LoraConfig, get_peft_model
+from trl import SFTTrainer
 
 
 def apply_lora(model, r=8, alpha=32, dropout=0.05):
@@ -18,32 +18,27 @@ def apply_lora(model, r=8, alpha=32, dropout=0.05):
 
 def fine_tune(model, tokenizer, dataset, output_dir="./results"):
 
-    def tokenize_fn(example):
-        return tokenizer(
-            example["text"],
-            truncation=True,
-            padding="max_length",
-            max_length=256
-        )
-
-    tokenized_ds = dataset.map(tokenize_fn, batched=True)
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
 
     training_args = TrainingArguments(
         output_dir=output_dir,
         num_train_epochs=2,
-        per_device_train_batch_size=2,
+        per_device_train_batch_size=1,
         gradient_accumulation_steps=4,
         learning_rate=2e-4,
-        fp16=True,
+        fp16=False, #mudar
         logging_steps=10,
-        save_strategy="epoch"
+        save_strategy="epoch",
     )
 
-    trainer = Trainer(
+    trainer = SFTTrainer(
         model=model,
         args=training_args,
-        train_dataset=tokenized_ds
+        train_dataset=dataset['train'],
+        eval_dataset=dataset['validation']
     )
 
     trainer.train()
     model.save_pretrained(f"{output_dir}/lora_model")
+    tokenizer.save_pretrained(f"{output_dir}/lora_model")
