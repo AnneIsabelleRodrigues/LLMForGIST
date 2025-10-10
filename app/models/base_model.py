949312706ffc1e65bd27d4bd4d8a68007e476729
Anne Isabelle Rodrigues de Carvalho <anneisabelle.rodrigues@outlook.com>
2025-10-10 20:46:24 -0300

    Alterando o modelo base

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -3,24 +3,35 @@ from huggingface_hub import login
 
 from app.config import HUGGINGFACE_TOKEN
 
-def load_base_model(model_name="Qwen/Qwen1.5-0.5B", load_in_8bit=True): #Qwen/Qwen2.5-7B
+def load_base_model(model_name="Qwen/Qwen3-4B-Instruct-2507", load_in_4bit: bool = False): #Qwen/Qwen2.5-7B
+
     login(HUGGINGFACE_TOKEN)
-    tokenizer = AutoTokenizer.from_pretrained(model_name, token=HUGGINGFACE_TOKEN, trust_remote_code=True)
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name,
+        token=HUGGINGFACE_TOKEN,
+        trust_remote_code=True
+    )
 
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
         tokenizer.pad_token_id = tokenizer.eos_token_id
 
-    bnb_config = BitsAndBytesConfig(
-        load_in_8bit=True
-    )
+    quant_config = None
+    if load_in_4bit:
+        quant_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype="float16"
+        )
 
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
-        quantization_config=bnb_config,
+        trust_remote_code=True,
+        quantization_config=quant_config,
         device_map="auto",
-        token=HUGGINGFACE_TOKEN,
-        trust_remote_code=True
+        token=HUGGINGFACE_TOKEN
     )
 
     model.config.pad_token_id = tokenizer.pad_token_id
